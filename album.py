4fce3248bf0f23d795d9fdf4a853ba526d5c9a29
rodrigr <rj.garcia.ribeiro@gmail.com>
2020-11-19 09:47:27 -0300

    ejercicio 1.1 terminado

---
+++ album.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 20:17:40 2019
+
+@author: rodri
+"""
+
+# importando paquetes necesarios
+
+import random
+
+import numpy as np
+
+# pruebas
+
+x = random.random()
+
+a = np.arange(10)
+
+len(a)
+
+a[:3]
+
+random.shuffle(a)
+
+np.mean(a)
+
+random.randint(1, 10)
+
+miSet = set()
+
+miSet.add(1)
+miSet.add(2)
+miSet.update([1,4,5])
+miSet.add(1)
+
+#Primera simplificación
+
+##1
+
+
+figu = random.randint(1,6)
+
+print("tu figurita es la número: % s" % (figu))
+
+
+
+
+
